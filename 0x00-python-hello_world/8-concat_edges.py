132dc646302c9e16445dee0360029cc271c787e0
hunterxcobby <solomonsefah13@gmail.com>
2023-10-02 14:48:03 +0000

    print object-oriented programming with Python, followed by a new line.

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-	language that combines remarkable power with very clear syntax"
+language that combines remarkable power with very clear syntax"
 arr = str.split(" ")
 str = arr[5] + " " + arr[6] + " " + arr[12] + " " + arr[0]
 print(str)
