83ec4c4306abd9fc6012ad884b04026bd0063d9c
hunterxcobby <solomonsefah13@gmail.com>
2023-10-02 15:19:27 +0000

    a function in C that checks if a singly linked list has a cycle in it.

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Function to check if there is cycle in list
+ * @list: linked list to check
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *hare = list;
+	listint_t *tortoise = list;
+
+	if (!list)
+		return (0);
+
+	while (1)
+	{
+		if (hare->next != NULL && hare->next->next != NULL)
+		{
+			hare = hare->next->next;
+			tortoise = slow->next;
+
+			/* Circle is found if nodes match*/
+			if (hare == tortoise)
+				return (1);
+		}
+		else
+			return (0);
+	}
+}
