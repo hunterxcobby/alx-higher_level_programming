a755f570b1f83194c547d83a53bfea2308195dfb
hunterxcobby <solomonsefah13@gmail.com>
2023-10-02 14:57:33 +0000

    solved the tab problem

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:67] + "with python"
+arr = str.split(" ")
+str = arr[5] + " " + arr[6] + " " + arr[12] + " " + arr[0]
 print(str)
