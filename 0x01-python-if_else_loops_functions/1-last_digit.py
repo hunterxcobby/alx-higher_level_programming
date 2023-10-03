7b6fd508200d040fbf077a36aca28f9515beaf08
hunterxcobby <solomonsefah13@gmail.com>
2023-10-03 23:04:52 +0000

    made file executable

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,16 +3,16 @@ import random
 number = random.randint(-10000, 10000)
 
 # We first convert the number to a string
-number_str = str(number)
+number_string = str(number)
 
 # Then extract the last digit
-last_digit = number_str[-1]
+last_digit = number_string[-1]
 
 # DOn't forget to first handle the case where the number is negative
 if number < 0:
     last_digit = "-" + last_digit
 
-# Print the original number
+# Print the original number and the alst digit 
 print(f"Last digit of {number} is {last_digit} and", end=" ")
 
 # Process conditions based on the last digit
