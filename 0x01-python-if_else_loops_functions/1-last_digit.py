4842ed56f1b7196afc6534ea235d79b39a0fdd60
hunterxcobby <solomonsefah13@gmail.com>
2023-10-03 22:58:56 +0000

    included a condition where the number is a negative number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,15 +8,17 @@ number_str = str(number)
 # Then extract the last digit
 last_digit = number_str[-1]
 
-# Print the original and last number
-print("Last digit of", end=" ")
-print(f"{number}", end=" ")
-print(f"is {last_digit}", end=" ")
+# DOn't forget to first handle the case where the number is negative
+if number < 0:
+    last_digit = "-" + last_digit
+
+# Print the original number
+print(f"Last digit of {number} is {last_digit} and", end=" ")
 
 # Process conditions based on the last digit
 if int(last_digit) > 5:
-    print(f"and is greater than 5")
+    print(f"is greater than 5")
 elif int(last_digit) == 0:
-    print(f"and is 0")
-elif int(last_digit) < 6 and int(last_digit) != 0:
-    print(f"and is less than 6 and not 0")
+    print(f"is 0")
+else:
+    print(f"is less than 6 and not 0")
