e7062aa6eff51a74c20e27be2833097e713e68a4
hunterxcobby <solomonsefah13@gmail.com>
2023-10-04 00:18:49 +0000

    why is the checker not checking a 6-print_comb3.py a 6-print_comb3.py ??

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
-for i in range(10):
-    for j in range(i+1, 10):
-        print("{:02d}, ".format(i*10 + j), end="")
-print()
+for i in range(0, 10):
+    for j in range((i+1), 10):
+        if (i != 8) or (j != 9):
+            print("{}{},".format(i, j), end=" ")
+        else:
+            print("{}{}".format(i, j))
