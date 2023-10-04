a8771ca370ecbf67b3eefa53a4589d0185579646
hunterxcobby <solomonsefah13@gmail.com>
2023-10-04 00:10:47 +0000

    implemented formatted string

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,5 +2,6 @@
 
 def uppercase(s):
     for char in s:
-        upper_char = chr(ord(char) - 32) if 97 <= ord(char) <= 122 else char
-        print(upper_char, end="")
+        print("[{}].format(chr({}))".format(
+            chr(ord(char) - 32) if 97 <= ord(char) <= 122 else char, ord(char)), end="")
+    print()
