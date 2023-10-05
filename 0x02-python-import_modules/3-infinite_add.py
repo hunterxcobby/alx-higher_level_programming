657c10017f1c0c1786c30d5f86ce3a3007a8ee1d
hunterxcobby <solomonsefah13@hmail.com>
2023-10-05 22:51:57 +0000

    a program that prints the result of the addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-import string
-print(string.ascii_uppercase)
+
+if __name__ == "__main__":
+    import sys
+
+    result = 0  # Initialize a variable to store the sum
+    arg_count = len(sys.argv) - 1
+
+    for i in range(arg_count):
+        result += (int(sys.argv[i + 1]))
+    print("{:d}".format(result))
