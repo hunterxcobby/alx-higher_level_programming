576e75526dc4f7042d6d2ef17575fe10dd7e323c
hunterxcobby <solomonsefah13@gmail.com>
2023-10-25 01:09:00 +0000

    a class Square that defines a square by: (based on 4-square.py)

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+"""
+Create class Square that defines a square by private instance attribute: size
+And a Public instance method: def area(self):
+that returns the current square area
+Methods Getter and Setter properties for size.
+property def size(self): to retrieve it
+property setter def size(self, value): to set it:
+Method my_print prints the square using "#".
+"""
+
+
+class Square:
+    """
+    Instantiating the variables self and size.
+    Raising errors if conditions are not met.
+    and print square using '#'.
+    """
+    def __init__(self, size=0):
+        # Initialize private attribute
+        self.__size = size
+
+    @property  # property to retrieve size (Getter method)
+    def size(self):
+        return self.__size
+
+    @size.setter  # Setter method for size
+    def size(self, value):
+        # Check if value is an integer
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        # Check if value is >= 0
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        # Update the private instance attribute
+        self.__size = value
+
+    def area(self):
+        """
+        Calculates area of square
+        Returns: area
+        """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
