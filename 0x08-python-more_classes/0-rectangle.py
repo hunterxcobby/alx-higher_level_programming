630440318cfad287f9ccc126cd449422c4095e38
hunterxcobby <solomonsefah13@gmail.com>
2023-10-30 22:18:15 +0000

    Write an empty class Rectangle that defines a rectangle:

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Class that defines a Rectangle.
+Empty class. Pass the ball to the next one, please.
+"""
+
+
+class Rectangle:
+    """An empty class funtion.
+    """
+    pass
