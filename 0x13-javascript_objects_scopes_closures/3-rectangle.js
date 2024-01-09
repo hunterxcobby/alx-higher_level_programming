5a34a15c143b89d535d477c17c18e90890033bf6
hunterxcobby <solomonsefah13@gmail.com>
2024-01-09 22:19:46 +0000

    a class Rectangle that defines a rectangle:

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let rectChar; // declare var to act as container for printing charcaters
+    for (let i = 0; i < this.height; i++) {
+      // let the variable be an empty string
+      rectChar = '';
+      for (let j = 0; j < this.width; j++) {
+        // append the X char to the variable in each iteration
+        rectChar += 'X';
+      }
+      console.log(rectChar);
+    }
+  }
+};
