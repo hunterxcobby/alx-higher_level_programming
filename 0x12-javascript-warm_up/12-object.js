9050dcb59086bc9279659b4a85e5dfd9f82911ee
hunterxcobby <solomonsefah13@gmail.com>
2024-01-12 09:35:52 +0000

    Update this script to replace the value 12 with 89:fix semistandard

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  myObject.value = 89;
-  console.log(myObject);
-  
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
