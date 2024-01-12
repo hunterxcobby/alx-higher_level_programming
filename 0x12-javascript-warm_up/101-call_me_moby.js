ffd6b3fe9768e8e8b228c4aedecfa1a251f95f52
hunterxcobby <solomonsefah13@gmail.com>
2024-01-12 09:45:13 +0000

    a function that executes x times a function: fix semistndard.

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.callMeMoby = function (x, theFunction) {
-    for (let y = 0; y < x; y++) {
-      theFunction();
-    }
-  };
+  for (let y = 0; y < x; y++) {
+    theFunction();
+  }
+};
