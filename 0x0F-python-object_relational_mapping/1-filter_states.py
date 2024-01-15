845cfa877478a7c37ff908bc47ece481f474fa1f
hunterxcobby <solomonsefah13@gmail.com>
2024-01-15 18:48:10 +0000

    a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa:

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-Script that lists all states from the database hbtn_0e_0_usa.
+Script that lists all states with a name starting with 'N'
+from the database hbtn_0e_0_usa.
 Parameters for script: mysql username, mysql password, database name.
 Must use the `MySQLdb` module.
 Script should connect to a MySQL server runnimg on `localhost` at port `3306`
@@ -9,20 +10,20 @@ Results must be in ascending order by `states.id`.
 Code should not be executed when imported.
 """
 
-import MySQLdb
 from sys import argv
+import MySQLdb
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # establishing a secure connection to the MySQL server
     db = MySQLdb.connect(
-        host='localhost',
+        host="localhost",
         port=3306,
         user=argv[1],
         passwd=argv[2],
         db=argv[3]
     )
 
-    # creating a cursor object to execute SQL queries
+    # createing a cursor object to execute SQL queries
     cursor = db.cursor()
 
     # executing the cursor to retrieve states sorted by id
@@ -33,7 +34,8 @@ if __name__ == '__main__':
 
     # display/print them out
     for state in states:
-        print(state)
+        if state[1][0] == 'N':
+            print(state)
 
     # closing the cursor and database connection
     cursor.close()
