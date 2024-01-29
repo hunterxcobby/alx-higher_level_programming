35e5e253a1630925a9f9aefbf90ffe83204b28a2
hunterxcobby <solomonsefah13@gmail.com>
2024-01-29 19:36:20 +0000

    cURL to the end

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+# Bash script that takes a URL, sends a GET request to the URL
+# and displays the body of the reponse, display only body of a 200 status code response
+curl -sL "$1"
