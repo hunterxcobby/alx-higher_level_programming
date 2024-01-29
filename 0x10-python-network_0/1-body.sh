30326b62fe35f12d52be2efd00052580841cc13f
hunterxcobby <solomonsefah13@gmail.com>
2024-01-29 19:40:48 +0000

    all scripts should be exactly three lines

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes a URL, sends a GET request to the URL
+# Bash script that takes a URL, sends a GET request to the URL, and displays the body of the reponse, display only body of a 200 status code response
 curl -sL "$1"
