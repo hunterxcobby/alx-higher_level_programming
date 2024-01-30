e17ad71c9ad6aca3898a474e3dea3724b7b9205b
hunterxcobby <solomonsefah13@gmail.com>
2024-01-30 12:39:48 +0000

    fix the option

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a POST request to the URL with specified parameters and display the body of the response
-curl -sX POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
