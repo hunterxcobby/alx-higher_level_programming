2e0d3d3e688f115bc7684af25169e5844c0336ac
hunterxcobby <solomonsefah13@gmail.com>
2024-01-30 18:25:34 +0000

    fix pycodestyle

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     url = "https://api.github.com/user"
 
     response = requests.get(url, auth=(username, password))
-    
+
     if response.status_code == 200:
         print(response.json()["id"])
     else:
