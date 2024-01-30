eeb175dc98e959f2d1efd4a82933ed8c53715841
hunterxcobby <solomonsefah13@gmail.com>
2024-01-30 13:05:39 +0000

    line must be exactly three lines

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 # Check if the number of arguments is not equal to 2
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <URL> <JSON_FILE>"
-    exit 1
-fi
-
-# Check if the JSON file exists
-if [ ! -f "$2" ]; then
-    echo "JSON file '$2' does not exist."
-    exit 1
-fi
-
-# Validate the JSON syntax
-if ! jq . "$2" >/dev/null 2>&1; then
-    echo "Not a valid JSON"
-    exit 1
-fi
-
-# Send a JSON POST request to the URL with the contents of the JSON file
-curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
