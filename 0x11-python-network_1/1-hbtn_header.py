711ea5b8b8e25e4cbed0c756258d9c617bd7b06c
hunterxcobby <solomonsefah13@gmail.com>
2024-01-30 17:52:37 +0000

    document module

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,6 +2,10 @@
 import urllib.request
 import sys
 
+"""Displays the X-Request-Id header variable of a request to a given URL.
+Usage: ./1-hbtn_header.py <URL>
+"""
+
 url = sys.argv[1]
 
 with urllib.request.urlopen(url) as response:
