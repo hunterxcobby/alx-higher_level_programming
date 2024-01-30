4cf3d61d5e82dc96dc70a4a6f7dcf3b914e6c1bf
hunterxcobby <solomonsefah13@gmail.com>
2024-01-30 18:24:17 +0000

    a Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Python script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+password in this case would be a personal access token
+Usage: ./10-my_github.py <GitHub username> <GitHub password>
+  - Uses Basic Authentication to access the ID.
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    url = "https://api.github.com/user"
+
+    response = requests.get(url, auth=(username, password))
+    
+    if response.status_code == 200:
+        print(response.json()["id"])
+    else:
+        print(None)
