118be1096ecc43d0c87a4d7151d90fa353a55ced
hunterxcobby <24598981-hunterxcobby@users.noreply.replit.com>
2024-02-20 10:54:17 +0000

    script that writes a string to a file.

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
