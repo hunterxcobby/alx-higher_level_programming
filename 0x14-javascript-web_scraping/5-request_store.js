c4a8d4adc925f78399d5202d0d5c48e74f28db87
hunterxcobby <24598981-hunterxcobby@users.noreply.replit.com>
2024-02-20 10:56:29 +0000

    script that gets the contents of a webpage and stores it in a file.

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+});
